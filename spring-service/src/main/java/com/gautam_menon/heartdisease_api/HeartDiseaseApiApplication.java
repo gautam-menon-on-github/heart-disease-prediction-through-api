79b994f10e682d0fda402110ffa36394b9bf28bc
GautamMenon <officially.gm11@gmail.com>
2026-08-08 16:28:05 +0530

    chore: added basic scaffold for spring-service

---
+++ spring-service/src/main/java/com/gautam_menon/heartdisease_api/HeartDiseaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.gautam_menon.heartdisease_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartDiseaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartDiseaseApiApplication.class, args);
+	}
+
+}
---
+++ spring-service/src/test/java/com/gautam_menon/heartdisease_api/HeartDiseaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautam_menon.heartdisease_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartDiseaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
